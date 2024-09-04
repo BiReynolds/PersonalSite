2e45c574d10fe7e11c8fa9301626c2464fbd18bf
BiReynolds <brriii77@gmail.com>
2024-09-03 22:45:47 -0400

    bleh

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://bireynolds.github.io'
+    site: 'https://bireynolds.github.io',
+    base: '/PersonalSite',
 });
