1019c6fc34f600b18607d207d83c98db9630426d
BiReynolds <94270364+BiReynolds@users.noreply.github.com>
2024-09-06 20:58:30 +0000

    Changed config to ensure local env file structure matches site's file structure

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://bireynolds.github.io',
+    base: '/PersonalSite',
+    trailingSlash: 'never'
 });
