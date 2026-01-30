916b3da8348d862478156c0e92328295a30f8453
BiReynolds <brriii77@gmail.com>
2026-01-29 22:51:04 -0500

    initial commit - Landing Page is functional and somewhat styled, and there is the beginnings of a ConwaysLife implementation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { LandingPage } from './Pages/landing-page/landing-page';
+import { ConwaysLife } from './Pages/conways-life/conways-life';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: LandingPage },
+  { path: 'ConwaysLife', component: ConwaysLife }
+];
